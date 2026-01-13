b0f9fff9b187448a48984575180d9604933e2dd3
FranBonilla88 <xfbonvar338@ieshnosmachado.org>
2026-01-13 22:43:48 +0100

    Realizando pruebas con el fronted y parte de backend perfecta

--- Proyecto_NodeJs/index.js
+++ Proyecto_NodeJs/index.js
@@ -12,7 +12,7 @@ const patientRoutes = require('./routes/patientRoutes');
 // INICIALIZACIÓN
 // ============================================
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // ============================================
 // MIDDLEWARE
